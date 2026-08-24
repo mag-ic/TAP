51efb4af2301cc2ccf40ac89566727c8eb26bbee
mag-ic <mag-ic@users.noreply.github.com>
2026-08-24 14:52:50 +0100

    feat: populate database and mock lists with Firestore application data

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (!localStorage.getItem('tap_db_firebase_loaded')) {
+  localStorage.removeItem('tap_partners');
+  localStorage.removeItem('tap_stock');
+  localStorage.removeItem('tap_sav_tickets');
+  localStorage.removeItem('tap_cheques');
+  localStorage.removeItem('tap_transactions');
+  localStorage.setItem('tap_db_firebase_loaded', 'true');
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
